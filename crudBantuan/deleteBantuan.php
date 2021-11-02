2a119bee7e135bc063326a3d42c67b43c357ae88
salsasabielar <salsa12rosyada@gmail.com>
2021-11-02 21:19:56 +0700

    tambah crud baru

--- crudBantuan/deleteBantuan.php
+++ crudBantuan/deleteBantuan.php
@@ -1,6 +1,6 @@
 <?php 
 include '../crudManageUser/config.php';
-$id_penerima = $_GET['id_bantuan'];
+$id_bantuan = $_GET['id_bantuan'];
 mysqli_query($koneksi,"DELETE FROM bantuan WHERE id_bantuan='$id_bantuan'")or die(mysqli_error($koneksi));
  
 header("location:../crudBantuan/bantuan.php");
