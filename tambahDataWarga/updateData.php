bb6b66fbdaffc2b06abc36f2467ac901a240561c
salsasabielar <salsa12rosyada@gmail.com>
2021-11-21 19:31:52 +0700

    edit filter

--- tambahDataWarga/updateData.php
+++ tambahDataWarga/updateData.php
@@ -20,17 +20,21 @@ if (mysqli_query($koneksi,"DELETE FROM kriteria_warga WHERE id_warga='$id_warga'
       }    
     
     }
-    
-     header("location:tambahData.php");
-    
-//     // if(mysqli_query ($koneksi, "INSERT INTO kriteria_warga (id_kriteria, id_warga) values ($ceka[$i], $id_warga)")) {
-//     //     
-//     // } else{
-//     //  echo mysqli_error($koneksi);
-//     // }            
-    
-// } else{
-//     echo mysqli_error($koneksi);
+    $result = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM kriteria_warga WHERE id_warga = $id_warga");
+        $row = mysqli_fetch_array($result);
+        $count = $row['total'];
+        echo $count;
+        $status = "";
+                        if($count >= 11 ){$status='Sangat Layak';}
+                        else if($count <= 10 && $count >= 6 ){$status='Layak';}
+                        else {$status='Tidak Layak';}
+                        
+                    echo $status;
+        if(mysqli_query($koneksi,"UPDATE warga SET status='$status' WHERE id_warga='$id_warga'")){
+           header("location:tambahData.php");
+           
+        }
+
 }
  
 
