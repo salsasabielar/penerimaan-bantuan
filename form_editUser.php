55bd6f8fb2ca6363ed8bdb927f4abb65ca29f585
salsasabielar <salsa12rosyada@gmail.com>
2021-12-05 22:39:22 +0700

    edit ui

--- form_editUser.php
+++ form_editUser.php
@@ -18,7 +18,7 @@
   <div id="wrapper">
     <!-- Sidebar -->
     <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="sidebar-brand-icon">
           <img src="img/logo/SSIB.png">
         </div>        
@@ -59,7 +59,7 @@
         </a>        
       </li>
       <li class="nav-item">
-        <a class="nav-link collapsed" href="../scan/validasiPenerima">
+        <a class="nav-link collapsed" href="scan/validasiPenerima">
           <i class="fas fa-fw fa-columns"></i>
           <span>Scanner</span>
         </a>
--- scan/validasiPenerima/index.php
+++ scan/validasiPenerima/index.php
@@ -1,53 +1,181 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-<meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>Validasi Data Penerima</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/font-awesome.css">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<!-- <link rel="icon"  href="../assets/img/logo.png"> -->
-<body>
-<nav class="navbar navbar-default">
-  <div class="container">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="./">Validasi Data Penerima</a>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="../../img/logo/logo-SIB.png" rel="icon">
+  <title>Sistem Informasi Penerimaan Bantuan</title>
+  <link href="../../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="../../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="../../css/ruang-admin.min.css" rel="stylesheet">
+  
+</head>
+
+<body id="page-top">
+  <div id="wrapper">
+    <!-- Sidebar -->
+    <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="../../index.php">
+        <div class="sidebar-brand-icon">
+          <img src="../../img/logo/SSIB.png">
+        </div>        
+      </a>
+      <li class="nav-item active">
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">       
+      </div>
+      <li class="nav-item">
+        <a class="nav-link" href="../../index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>Dashboard</span>
+        </a>        
+      </li>
+      <hr class="sidebar-divider">
+      <div class="sidebar-heading">
+        Menu
+      </div>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseBootstrap"
+          aria-expanded="true" aria-controls="collapseBootstrap">
+          <i class="far fa-fw fa-window-maximize"></i>
+          <span>Data Warga</span>
+        </a>
+        <div id="collapseBootstrap" class="collapse" aria-labelledby="headingBootstrap" data-parent="#accordionSidebar">
+          <div class="bg-white py-2 collapse-inner rounded">
+            <a class="collapse-item" href="../../tambahDataWarga/tambahData.php">Tambah Data</a>
+            <a class="collapse-item" href="../../hapus.php">Data Terhapus</a>
+            <a class="collapse-item" href="../../generateQrcode.php">Generate QR-Code</a>
+          </div>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../crudKriteria/tambahKriteria.php">
+          <i class="fab fa-fw fa-wpforms"></i>
+          <span>Kriteria</span>
+        </a>        
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="index.php">
+          <i class="fas fa-fw fa-columns"></i>
+          <span>Scanner</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link collapsed" href="../../laporan.php">
+          <i class="fas fa-fw fa-table"></i>
+          <span>Laporan</span>
+        </a>        
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../../manageUser.php">
+          <i class="fas fa-fw fa-palette"></i>
+          <span>Manajemen User</span>
+        </a>
+      </li>
+      <hr class="sidebar-divider">    
+      <li class="nav-item"> 
+        <a class="nav-link" href="../../logout.php">
+          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400 "></i>  
+          <span>Keluar</span>
+        </a>
+      </li>
+    </ul>
+    <!-- Sidebar -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <!-- TopBar -->
+        <nav class="navbar navbar-expand navbar-light bg-navbar topbar mb-4 static-top">
+          <button id="sidebarToggleTop" class="btn btn-link rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+          
+        </nav>
+        <!-- Topbar -->
+
+        <!-- Container Fluid-->
+        <div class="container-fluid" id="container-wrapper">
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Scanner</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="../../index.php">Home</a></li>
+              <li class="breadcrumb-item">Scanner</li>
+            </ol>
+          </div>
+
+          <!-- Row -->
+          <div class="row">
+            <!-- Datatables -->
+            <div class="col-lg-12">
+              <!-- Simple Tables -->
+              <div class="card">
+                
+                <div class="table-responsive">
+                <table class="table align-items-center table-flush">
+                    <thead class="thead-light">
+                      <tr>
+                        <th>Arahkan Kode QR Ke Kamera!</th>
+                        
+                      </tr>
+                    </thead>
+                    <tr>
+                      <th>
+
+                      <div class="container">
+                        <div class="row">
+                          <div class="col-md-4 col-md-offset-4">
+                            <div class="panel panel-danger">
+                              <!-- <div class="panel-heading">
+                                <h3 class="panel-title">Arahkan Kode QR Ke Kamera!</h3>
+                              </div> -->
+                              <div class="panel-body text-center" >
+                                <canvas></canvas>
+                                <hr>
+                                <select></select>
+                              </div>
+                              <div class="panel-footer">
+                                  <!-- <center><a class="btn btn-danger" href="../tambahDataWarga/tambahData.php">Kembali</a></center> -->
+                              </div>
+                            </div>
+                          </div>
+
+                        </div>
+                      </div>
+
+                      </th>
+                    </tr>
+                    
+                  </table>
+                </div>
+                <div class="card-footer"></div>
+              </div>
+            </div>
+          </div>
+          <!--Row-->
+          
+        </div>
+      </div>
+      
     </div>
   </div>
-</nav>
 
-<div class="container">
-<div class="row">
-  <div class="col-md-4 col-md-offset-4">
-    <div class="panel panel-danger">
-      <div class="panel-heading">
-        <h3 class="panel-title">Arahkan Kode QR Ke Kamera!</h3>
-      </div>
-      <div class="panel-body text-center" >
-        <canvas></canvas>
-        <hr>
-        <select></select>
-      </div>
-      <div class="panel-footer">
-          <!-- <center><a class="btn btn-danger" href="../">Kembali</a></center> -->
-      </div>
-    </div>
-  </div>
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
 
-</div>
-</div>
+  <script src="../../vendor/jquery/jquery.min.js"></script>
+  <script src="../../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="../../vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="../../js/ruang-admin.min.js"></script>
+  <script src="../../vendor/chart.js/Chart.min.js"></script>
+  <script src="../../js/demo/chart-area-demo.js"></script>  
 
-<!-- Js Lib -->
-<script type="text/javascript" src="js/jquery.js"></script>
+  <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/qrcodelib.js"></script>
 <script type="text/javascript" src="js/webcodecamjquery.js"></script>
 <script type="text/javascript">
@@ -84,5 +212,10 @@
     });
 
 </script>
+
+
+
+
 </body>
+
 </html>
